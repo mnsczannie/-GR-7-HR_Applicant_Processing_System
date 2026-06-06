4df0ab10e3927a5ce8254d9d9f6d16ea6472108e
mxrkpxtrxckocxmpx <markpatrickocampo@gmail.com>
2026-06-06 17:23:39 +0800

    Added HRApplicantForms with applicant list and a "double-click to review" feature.

--- Forms/HR/HRApplicantForms.Designer.cs
+++ Forms/HR/HRApplicantForms.Designer.cs
@@ -2,38 +2,56 @@
 {
     partial class HRApplicantForms
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvApplicants = new System.Windows.Forms.DataGridView();
+            this.lblTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvApplicants)).BeginInit();
+            this.SuspendLayout();
+
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(12, 15);
+            this.lblTitle.Text = "Applicant List";
+
+            this.dgvApplicants.AllowUserToAddRows = false;
+            this.dgvApplicants.AllowUserToDeleteRows = false;
+            this.dgvApplicants.ReadOnly = true;
+            this.dgvApplicants.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvApplicants.Location = new System.Drawing.Point(12, 50);
+            this.dgvApplicants.MultiSelect = false;
+            this.dgvApplicants.Name = "dgvApplicants";
+            this.dgvApplicants.RowHeadersVisible = false;
+            this.dgvApplicants.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvApplicants.Size = new System.Drawing.Size(660, 380);
+            this.dgvApplicants.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvApplicants_CellDoubleClick);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "HRApplicantForms";
+            this.ClientSize = new System.Drawing.Size(684, 451);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.dgvApplicants);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "HRApplicantForms";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Applicant List";
+            this.Load += new System.EventHandler(this.HRApplicantForms_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvApplicants)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        #endregion
+        private System.Windows.Forms.DataGridView dgvApplicants;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
--- Forms/HR/HRApplicantForms.cs
+++ Forms/HR/HRApplicantForms.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using HRApplicantSystem.Helpers;
 
 namespace HRApplicantSystem.Forms.HR
 {
@@ -15,6 +11,57 @@ namespace HRApplicantSystem.Forms.HR
         public HRApplicantForms()
         {
             InitializeComponent();
+        }
+
+        private void HRApplicantForms_Load(object sender, EventArgs e)
+        {
+            LoadApplicants();
+        }
+
+        private void LoadApplicants()
+        {
+            string query = @"
+                SELECT ap.application_id,
+                       a.first_name + ' ' + a.last_name  AS name,
+                       jv.title                          AS position,
+                       ap.status,
+                       CONVERT(varchar, ap.submitted_at, 101) AS date_submitted
+                FROM   applications ap
+                JOIN   applicants    a  ON ap.applicant_id   = a.applicant_id
+                JOIN   job_vacancies jv ON ap.job_vacancy_id = jv.job_vacancy_id
+                ORDER BY ap.submitted_at DESC";
+
+            var dt = new DataTable();
+            using (var conn = DatabaseHelper.GetConnection())
+            using (var cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                using (var adapter = new SqlDataAdapter(cmd))
+                    adapter.Fill(dt);
+            }
+
+            dgvApplicants.DataSource = dt;
+
+            if (dgvApplicants.Columns["application_id"] != null)
+                dgvApplicants.Columns["application_id"].Visible = false;
+
+            if (dgvApplicants.Columns["name"] != null)
+                dgvApplicants.Columns["name"].HeaderText = "Name";
+            if (dgvApplicants.Columns["position"] != null)
+                dgvApplicants.Columns["position"].HeaderText = "Position";
+            if (dgvApplicants.Columns["status"] != null)
+                dgvApplicants.Columns["status"].HeaderText = "Status";
+            if (dgvApplicants.Columns["date_submitted"] != null)
+                dgvApplicants.Columns["date_submitted"].HeaderText = "Date Submitted";
+        }
+
+        private void dgvApplicants_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            var reviewForm = new frmApplicantReview();
+            reviewForm.Show();
+            this.Close();
         }
     }
 }
