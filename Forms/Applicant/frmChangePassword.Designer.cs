7d02fa776992604af8cbdb8d87201a54c29c7eb1
mnsczannie <zanneantonette09@gmail.com>
2026-06-22 08:08:10 +0800

    Change Password

--- Forms/Applicant/frmChangePassword.Designer.cs
+++ Forms/Applicant/frmChangePassword.Designer.cs
@@ -28,94 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblCurrentPassword = new System.Windows.Forms.Label();
-            this.lblNewPassword = new System.Windows.Forms.Label();
-            this.lblConfirmPassword = new System.Windows.Forms.Label();
-            this.txtCurrentPassword = new System.Windows.Forms.TextBox();
-            this.txtNewPassword = new System.Windows.Forms.TextBox();
-            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
-            this.btnSavePassword = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSubtitle = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkShowPass = new System.Windows.Forms.CheckBox();
+            this.btnChangePass = new System.Windows.Forms.Button();
+            this.txtNewPass = new System.Windows.Forms.TextBox();
+            this.txtConfirmPass = new System.Windows.Forms.TextBox();
+            this.txtCurrentPass = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblCurrentPassword
+            // lblTitle
             // 
-            this.lblCurrentPassword.AutoSize = true;
-            this.lblCurrentPassword.Location = new System.Drawing.Point(192, 48);
-            this.lblCurrentPassword.Name = "lblCurrentPassword";
-            this.lblCurrentPassword.Size = new System.Drawing.Size(135, 20);
-            this.lblCurrentPassword.TabIndex = 0;
-            this.lblCurrentPassword.Text = "Current Password";
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.lblTitle.Location = new System.Drawing.Point(316, 84);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(252, 29);
+            this.lblTitle.TabIndex = 10;
+            this.lblTitle.Text = "Change Password";
             // 
-            // lblNewPassword
+            // lblSubtitle
             // 
-            this.lblNewPassword.AutoSize = true;
-            this.lblNewPassword.Location = new System.Drawing.Point(192, 94);
-            this.lblNewPassword.Name = "lblNewPassword";
-            this.lblNewPassword.Size = new System.Drawing.Size(113, 20);
-            this.lblNewPassword.TabIndex = 1;
-            this.lblNewPassword.Text = "New Password";
-            this.lblNewPassword.Click += new System.EventHandler(this.txtNewPassword_Click);
+            this.lblSubtitle.AutoSize = true;
+            this.lblSubtitle.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Italic);
+            this.lblSubtitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.lblSubtitle.Location = new System.Drawing.Point(318, 118);
+            this.lblSubtitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSubtitle.Name = "lblSubtitle";
+            this.lblSubtitle.Size = new System.Drawing.Size(267, 16);
+            this.lblSubtitle.TabIndex = 11;
+            this.lblSubtitle.Text = "Update the password for your account";
             // 
-            // lblConfirmPassword
+            // btnBack
             // 
-            this.lblConfirmPassword.AutoSize = true;
-            this.lblConfirmPassword.Location = new System.Drawing.Point(192, 140);
-            this.lblConfirmPassword.Name = "lblConfirmPassword";
-            this.lblConfirmPassword.Size = new System.Drawing.Size(172, 20);
-            this.lblConfirmPassword.TabIndex = 2;
-            this.lblConfirmPassword.Text = "Confirm New Password";
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Verdana", 10F);
+            this.btnBack.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.btnBack.Location = new System.Drawing.Point(1254, 20);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(150, 29);
+            this.btnBack.TabIndex = 12;
+            this.btnBack.Text = "← Back to Dashboard";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
-            // txtCurrentPassword
+            // groupBox1
             // 
-            this.txtCurrentPassword.Location = new System.Drawing.Point(466, 41);
-            this.txtCurrentPassword.Name = "txtCurrentPassword";
-            this.txtCurrentPassword.PasswordChar = '*';
-            this.txtCurrentPassword.Size = new System.Drawing.Size(150, 26);
-            this.txtCurrentPassword.TabIndex = 3;
-            this.txtCurrentPassword.UseSystemPasswordChar = true;
+            this.groupBox1.Controls.Add(this.chkShowPass);
+            this.groupBox1.Controls.Add(this.btnChangePass);
+            this.groupBox1.Controls.Add(this.txtNewPass);
+            this.groupBox1.Controls.Add(this.txtConfirmPass);
+            this.groupBox1.Controls.Add(this.txtCurrentPass);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Verdana", 10F);
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(92)))), ((int)(((byte)(153)))));
+            this.groupBox1.Location = new System.Drawing.Point(314, 158);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(687, 311);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Change Password";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // txtNewPassword
+            // chkShowPass
             // 
-            this.txtNewPassword.Location = new System.Drawing.Point(466, 88);
-            this.txtNewPassword.Name = "txtNewPassword";
-            this.txtNewPassword.PasswordChar = '*';
-            this.txtNewPassword.Size = new System.Drawing.Size(150, 26);
-            this.txtNewPassword.TabIndex = 4;
-            this.txtNewPassword.UseSystemPasswordChar = true;
+            this.chkShowPass.AutoSize = true;
+            this.chkShowPass.Location = new System.Drawing.Point(437, 210);
+            this.chkShowPass.Name = "chkShowPass";
+            this.chkShowPass.Size = new System.Drawing.Size(138, 21);
+            this.chkShowPass.TabIndex = 9;
+            this.chkShowPass.Text = "Show Password";
+            this.chkShowPass.UseVisualStyleBackColor = true;
+            this.chkShowPass.CheckedChanged += new System.EventHandler(this.chkShowPass_CheckedChanged);
             // 
-            // txtConfirmPassword
+            // btnChangePass
             // 
-            this.txtConfirmPassword.Location = new System.Drawing.Point(466, 134);
-            this.txtConfirmPassword.Name = "txtConfirmPassword";
-            this.txtConfirmPassword.PasswordChar = '*';
-            this.txtConfirmPassword.Size = new System.Drawing.Size(150, 26);
-            this.txtConfirmPassword.TabIndex = 5;
-            this.txtConfirmPassword.UseSystemPasswordChar = true;
+            this.btnChangePass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(92)))), ((int)(((byte)(153)))));
+            this.btnChangePass.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChangePass.ForeColor = System.Drawing.Color.White;
+            this.btnChangePass.Location = new System.Drawing.Point(301, 240);
+            this.btnChangePass.Name = "btnChangePass";
+            this.btnChangePass.Size = new System.Drawing.Size(230, 31);
+            this.btnChangePass.TabIndex = 8;
+            this.btnChangePass.Text = "Change Password";
+            this.btnChangePass.UseVisualStyleBackColor = false;
+            this.btnChangePass.Click += new System.EventHandler(this.btnChangePass_Click);
             // 
-            // btnSavePassword
+            // txtNewPass
             // 
-            this.btnSavePassword.Location = new System.Drawing.Point(466, 177);
-            this.btnSavePassword.Name = "btnSavePassword";
-            this.btnSavePassword.Size = new System.Drawing.Size(150, 39);
-            this.btnSavePassword.TabIndex = 6;
-            this.btnSavePassword.Text = "Save Password";
-            this.btnSavePassword.UseVisualStyleBackColor = true;
-            this.btnSavePassword.Click += new System.EventHandler(this.btnSavePassword_Click);
+            this.txtNewPass.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNewPass.Location = new System.Drawing.Point(302, 128);
+            this.txtNewPass.Name = "txtNewPass";
+            this.txtNewPass.PasswordChar = '•';
+            this.txtNewPass.Size = new System.Drawing.Size(230, 29);
+            this.txtNewPass.TabIndex = 7;
+            this.txtNewPass.TextChanged += new System.EventHandler(this.txtNewPass_TextChanged);
+            // 
+            // txtConfirmPass
+            // 
+            this.txtConfirmPass.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPass.Location = new System.Drawing.Point(302, 163);
+            this.txtConfirmPass.Name = "txtConfirmPass";
+            this.txtConfirmPass.PasswordChar = '•';
+            this.txtConfirmPass.Size = new System.Drawing.Size(230, 29);
+            this.txtConfirmPass.TabIndex = 6;
+            // 
+            // txtCurrentPass
+            // 
+            this.txtCurrentPass.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCurrentPass.Location = new System.Drawing.Point(302, 93);
+            this.txtCurrentPass.Name = "txtCurrentPass";
+            this.txtCurrentPass.PasswordChar = '•';
+            this.txtCurrentPass.Size = new System.Drawing.Size(230, 29);
+            this.txtCurrentPass.TabIndex = 5;
+            this.txtCurrentPass.TextChanged += new System.EventHandler(this.txtCurrentPass_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(302, 58);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(230, 29);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 14F);
+            this.label4.Location = new System.Drawing.Point(111, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Confirm Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 14F);
+            this.label3.Location = new System.Drawing.Point(111, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Current Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 14F);
+            this.label2.Location = new System.Drawing.Point(140, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "New Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14F);
+            this.label1.Location = new System.Drawing.Point(221, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email:";
             // 
             // frmChangePassword
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnSavePassword);
-            this.Controls.Add(this.txtConfirmPassword);
-            this.Controls.Add(this.txtNewPassword);
-            this.Controls.Add(this.txtCurrentPassword);
-            this.Controls.Add(this.lblConfirmPassword);
-            this.Controls.Add(this.lblNewPassword);
-            this.Controls.Add(this.lblCurrentPassword);
+            this.ClientSize = new System.Drawing.Size(1368, 741);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblSubtitle);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.groupBox1);
             this.Name = "frmChangePassword";
-            this.Text = "frmChangePassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change Password";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmChangePassword_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,12 +229,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblCurrentPassword;
-        private System.Windows.Forms.Label lblNewPassword;
-        private System.Windows.Forms.Label lblConfirmPassword;
-        private System.Windows.Forms.TextBox txtCurrentPassword;
-        private System.Windows.Forms.TextBox txtNewPassword;
-        private System.Windows.Forms.TextBox txtConfirmPassword;
-        private System.Windows.Forms.Button btnSavePassword;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSubtitle;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNewPass;
+        private System.Windows.Forms.TextBox txtConfirmPass;
+        private System.Windows.Forms.TextBox txtCurrentPass;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnChangePass;
+        private System.Windows.Forms.CheckBox chkShowPass;
     }
 }
